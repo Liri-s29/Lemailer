e5cdf8ef7ca9cdb476ee1e38b0489efdb7fa6118
srihari-zluri <srihari.s@zluri.com>
2023-07-04 11:42:31 +0530

    endpoint fix

--- src/services/fetchTrackingStatus.js
+++ src/services/fetchTrackingStatus.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const getEmailStats = async (userEmail) => {
 	try {
-		// const response = await axios.get("https://lemailer-backend.onrender.com/api/email-stats", {
-		const response = await axios.get("http://localhost:3001/api/email-stats", {
+		const response = await axios.get("https://lemailer-backend.onrender.com/api/email-stats", {
+		// const response = await axios.get("http://localhost:3001/api/email-stats", {
 			params: {
 				userEmail: userEmail, // Add userEmail as a query parameter
 			},
